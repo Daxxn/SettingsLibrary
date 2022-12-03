1ba02f54fb83cae1f52f4070fa5176eae0a02509
Cody Lantz <46167982+Daxxn@users.noreply.github.com>
2022-12-03 14:17:36 -0800

    Fix appName bug and update readme

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Settings Library
+
+A simple settings manager that saves settings in a JSON file. It will try to create a new folder in the users AppData folder with the name of the app. Then save the settings file there.
+
+## Usage With WPF
+
+Example Settings Model
+
+```cs
+using SettingsLibrary.Models;
+
+public class SettingsModel : ISettingsModel
+{
+  public string? SavePath { get; set; } = $@"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\Path\to\file.txt";
+  public string? LastSavePath { get; set; }
+  public int CurrrentTab { get; set; }
+}
+```
+
+App.xaml.cs
+
+```cs
+public partial class App : Application
+{
+  public static SettingsModel StateSettings { get; private set; } = null!;
+
+  protected override void OnStartup(StartupEventArgs e)
+  {
+    // Loads the settings from the JSON settings file.
+    StateSettings = SettingsManager.OnStartup<SettingsModel>("<AppName>");
+    // Other startup operations ...
+    base.OnStartup(e);
+  }
+
+  protected override void OnExit(ExitEventArgs e)
+  {
+    // Other shutdown operations ...
+
+    // Saves to the JSON settings file.
+    SettingsManager.OnExit(StateSettings, "<AppName>");
+    base.OnExit(e);
+  }
+}
+```
--- SettingsLibrary/SettingsManager.cs
+++ SettingsLibrary/SettingsManager.cs
@@ -7,18 +7,16 @@ namespace SettingsLibrary;
 public static class SettingsManager
 {
    #region Local Props
-   private static readonly string FileName = "settings.json";
-   public static string? AppName { get; set; }
+   private static readonly string _fileName = "settings.json";
    #endregion
 
    #region Methods
    public static T? OnStartup<T>(string appName) where T : ISettingsModel, new()
    {
-      AppName = appName;
       var appDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), appName);
       if (Directory.Exists(appDataPath))
       {
-         string settingsPath = Path.Combine(appDataPath, FileName);
+         string settingsPath = Path.Combine(appDataPath, _fileName);
          if (File.Exists(settingsPath))
          {
             return JsonReader.OpenJsonFile<T>(settingsPath);
@@ -32,10 +30,10 @@ public static class SettingsManager
       return new T();
    }
 
-   public static void OnExit<T>(T settings) where T : ISettingsModel, new()
+   public static void OnExit<T>(T settings, string appName) where T : ISettingsModel, new()
    {
-      if (AppName is null) return;
-      var settingsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), AppName, FileName);
+      if (string.IsNullOrEmpty(appName)) throw new ArgumentException(nameof(appName), "No name provided. Unable to find settings path.");
+      var settingsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), appName, _fileName);
       JsonReader.SaveJsonFile(settingsPath, settings, true);
    }
    #endregion
